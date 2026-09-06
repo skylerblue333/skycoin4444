b56d30688d180f82124918809f5a29e001c3da6f
Skyler Blue Spillers <92972770+skylerblue333@users.noreply.github.com>
2026-09-05 19:34:05 -0500

    chore: remove unused reflex-lab import

--- client/src/pages/GameCrash.tsx
+++ client/src/pages/GameCrash.tsx
@@ -20,7 +20,6 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Progress } from "@/components/ui/progress";
 
 type RoundState = "idle" | "running" | "resolved" | "finished";
 
